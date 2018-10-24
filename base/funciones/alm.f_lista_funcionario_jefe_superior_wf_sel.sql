c4495686703b8e5794ad9299cf76877aaeff279b
ismael2019 <irva2008_@hotmail.com>
2018-10-23 20:11:43 -0400

    ARCHIVOS BASE FALTANTES

--- base/funciones/alm.f_lista_funcionario_jefe_superior_wf_sel.sql
+++ base/funciones/alm.f_lista_funcionario_jefe_superior_wf_sel.sql
@@ -17,13 +17,13 @@ $body$
  DESCRIPCIÓN: 	Lista los fucionarios jefes superiores del funcionario
  AUTOR: 		Gonzalo Sarmiento Sejas
  FECHA:			19/04/2016
- COMENTARIOS:	
+ COMENTARIOS:
 ***************************************************************************
  HISTORIA DE MODIFICACIONES:
 
  DESCRIPCIÓN:
- AUTOR:       
- FECHA:      
+ AUTOR:
+ FECHA:
 
 ***************************************************************************/
 
@@ -36,7 +36,7 @@ $body$
 
 
   p_id_usuario integer,                                identificador del actual usuario de sistema
-  p_id_tipo_estado integer,                            idnetificador del tipo estado del que se quiere obtener el listado de funcionario  (se correponde con tipo_estado que le sigue a id_estado_wf proporcionado)                       
+  p_id_tipo_estado integer,                            idnetificador del tipo estado del que se quiere obtener el listado de funcionario  (se correponde con tipo_estado que le sigue a id_estado_wf proporcionado)
   p_fecha date = now(),                                fecha  --para verificar asginacion de cargo con organigrama
   p_id_estado_wf integer = NULL::integer,              identificador de estado_wf actual en el proceso_wf
   p_count boolean = false,                             si queremos obtener numero de funcionario = true por defecto false
@@ -50,24 +50,24 @@ $body$
 */
 
 DECLARE
-	g_registros  		record;    
+	g_registros  		record;
     v_consulta varchar;
     v_nombre_funcion varchar;
-    v_resp varchar;        
-    v_id_funcionario   integer;    
+    v_resp varchar;
+    v_id_funcionario   integer;
 
 BEGIN
   v_nombre_funcion ='alm.f_lista_funcionario_jefe_superior_wf_sel';
 
     --recuperamos la la obligacion de pago a partir del id_estado_wf en el proceso caja
-    
-    select 
+
+    select
       id_funcionario
     into
       v_id_funcionario
     from alm.tmovimiento
     where id_estado_wf = p_id_estado_wf;
-    
+    --RAISE EXCEPTION 'DATOS_: %, %, %, %, %', v_id_funcionario, p_filtro, p_limit, p_start, p_count;
     IF not p_count then
             raise notice 'llega1';
              v_consulta:='SELECT
@@ -78,31 +78,31 @@ BEGIN
                          FROM orga.vfuncionario fun WHERE fun.id_funcionario in (
                          select * from orga.f_get_aprobadores_x_funcionario(CURRENT_DATE,'|| v_id_funcionario ||',''todos'',''todos'',''1,2,3,4,5'') as
 						 (id_funcionario integer)) and '||p_filtro||'
-                         limit '|| p_limit::varchar||' offset '||p_start::varchar; 
+                         limit '|| p_limit::varchar||' offset '||p_start::varchar;
                      raise notice 'v_consulta %', v_consulta;
-             FOR g_registros in execute (v_consulta)LOOP     
+             FOR g_registros in execute (v_consulta)LOOP
                      RETURN NEXT g_registros;
              END LOOP;
-                      
+
       ELSE
               raise notice 'llega2';
                   v_consulta='select
                                   COUNT(fun.id_funcionario) as total
                                  FROM orga.vfuncionario fun WHERE fun.id_funcionario in (
                                  select * from orga.f_get_aprobadores_x_funcionario(CURRENT_DATE,'|| v_id_funcionario ||',''todos'',''todos'',''1,2,3,4,5'') as
-						 (id_funcionario integer)) and '||p_filtro;   
-                                          
-                   FOR g_registros in execute (v_consulta)LOOP     
+						 (id_funcionario integer)) and '||p_filtro;
+
+                   FOR g_registros in execute (v_consulta)LOOP
                      RETURN NEXT g_registros;
                    END LOOP;
-                       
-                       
+
+
     END IF;
-               
-        
-       
+
+
+
 EXCEPTION
-					
+
 	WHEN OTHERS THEN
 			v_resp='';
 			v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
@@ -110,7 +110,7 @@ EXCEPTION
 			v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
 			raise exception '%',v_resp;
 
-				        
+
 END;
 $body$
 LANGUAGE 'plpgsql'
